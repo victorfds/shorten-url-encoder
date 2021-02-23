a2985f45cce2c60e49dc95d3c8a6cbba4050ee0c
Victor Ferreira da Silva <victorxpro@yahoo.com>
2021-02-23 15:30:22 -0400

    ✅

--- src/dbCommands/handlerShorterUrl.ts
+++ src/dbCommands/handlerShorterUrl.ts
@@ -8,7 +8,7 @@ const dbCredentials = {
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
-  ssl: true,
+  ssl: { rejectUnauthorized: false },
 };
 
 const urlControl = {
